18a829f6630c60bfcf2007e5f98bfe66b563ba77
doppleware <roni.dover@gmail.com>
2019-09-02 23:38:43 -0700

    updated

--- terraform/rds-mysql-aurora-cluster/main.tf
+++ terraform/rds-mysql-aurora-cluster/main.tf
@@ -33,8 +33,8 @@ resource "aws_rds_cluster" "cluster" {
 
   scaling_configuration {
     auto_pause   = "true"
-    max_capacity = "2"
-    min_capacity = "8"
+    max_capacity = "8"
+    min_capacity = "2"
   }
 
   lifecycle {
