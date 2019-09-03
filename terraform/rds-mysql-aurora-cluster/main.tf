cb4f4c19391c81237c11d45a0a7f959a6b7156bf
doppleware <roni.dover@gmail.com>
2019-09-02 23:24:20 -0700

    updated

--- terraform/rds-mysql-aurora-cluster/main.tf
+++ terraform/rds-mysql-aurora-cluster/main.tf
@@ -1,21 +1,45 @@
 provider "aws" {
   region = "eu-west-1"
 }
-
-resource "aws_rds_cluster_instance" "cluster_instances" {
-  identifier         = "${var.sandbox_id}-instance"
-  cluster_identifier = "${aws_rds_cluster.cluster.id}"
-  instance_class     = "db.t2.medium"
-  db_subnet_group_name = "${aws_db_subnet_group.rds.id}"
-
-}
+//
+//resource "aws_rds_cluster_instance" "cluster_instances" {
+//  identifier         = "${var.sandbox_id}-instance"
+//  cluster_identifier = "${aws_rds_cluster.cluster.id}"
+//  instance_class     = "db.t2.medium"
+//  db_subnet_group_name = "${aws_db_subnet_group.rds.id}"
+//
+//}
+//
+//resource "aws_rds_cluster" "cluster" {
+//  cluster_identifier     = "${var.sandbox_id}-cluster"
+//  database_name          = "${var.db_name}"
+//  master_username        = "${var.username}"
+//  master_password        = "${var.password}"
+//  vpc_security_group_ids = ["${aws_security_group.rds.id}"]
+//  db_subnet_group_name = "${aws_db_subnet_group.rds.id}"
+//  skip_final_snapshot    = true
+//}
 
 resource "aws_rds_cluster" "cluster" {
   cluster_identifier     = "${var.sandbox_id}-cluster"
   database_name          = "${var.db_name}"
   master_username        = "${var.username}"
   master_password        = "${var.password}"
+  engine_version         = "5.7.12"
+  engine_mode            = "serverless"
+  engine                 = "aurora"
   vpc_security_group_ids = ["${aws_security_group.rds.id}"]
   db_subnet_group_name = "${aws_db_subnet_group.rds.id}"
   skip_final_snapshot    = true
+
+  scaling_configuration {
+    auto_pause   = "true"
+    max_capacity = "2"
+    min_capacity = "8"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
 }
